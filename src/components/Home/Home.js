a7fb3f5a7d62e9b36c66437f4ee8d8ea10a4211c
balaji <114200498+codewithbalaji@users.noreply.github.com>
2024-09-27 20:35:58 +0530

    test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,7 @@ import img1 from "assets/home.png";
 import {showNotification, setNotificationCount, clearNotificationCount, askNotificationPermission} from 'gost-next-pwa-notification'
 
 const Home = () => {
+  askNotificationPermission()
   showNotification('hunger free', 'testing text', '/icon-192x192.png', false, 'default')
   const history = useNavigate();
 
